255a76c9c258a7d731acf3c607acb0ccd4f5cbea
murari2209 <shrivastava.murari@gmail.com>
2026-03-27 03:23:19 +0530

    initial setup

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+  page_title="Murari Portfolio",
+  layout="wide"
+)
+
+st.title("Murari Portfolio")
+st.subheader("Python beckend developer")
+
+st.write("Welcome to my portfolio!")
