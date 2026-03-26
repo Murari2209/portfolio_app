7796c1c6aac7cd9d3ad63140718ad745a54224b9
murari2209 <shrivastava.murari@gmail.com>
2026-03-27 04:18:48 +0530

    added resume and profile pic

--- app.py
+++ app.py
@@ -9,3 +9,24 @@ st.title("Murari Portfolio")
 st.subheader("Python beckend developer")
 
 st.write("Welcome to my portfolio!")
+
+
+col1, col2 = st.columns([1, 2])
+
+with col1:
+    st.image("assets/Murari_Profile_Image.png", width=200)
+
+with col2:
+    st.title("Murari Shrivastava")
+    st.subheader("Python Backend Developer")
+    st.write("Building scalable backend systems.")
+
+with open("assets/Murari Shrivastava_Senior_Software_Developer.pdf", "rb") as pdf_file:
+    PDFbyte = pdf_file.read()
+
+st.download_button(
+    label="Download Resume",
+    data=PDFbyte,
+    file_name="Murari Shrivastava_Senior_Software_Developer.pdf",
+    mime="application/octet-stream"
+)
